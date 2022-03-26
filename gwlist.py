568a913bc75d1237ebbbeff4a87aa481a61b9834
GeraldoMBorgesF <geraldomborgesf@gmail.com>
2022-03-26 09:11:07 -0300

    permuta as letras da palavra fornecida no input e as imprime

--- gwlist.py
+++ gwlist.py
@@ -4,7 +4,7 @@ import itertools
 
 string = input('Palavra a ser permutada: ')
 
-# permuta as letras da palavra fornecida no input e as imprime
+#permuta as letras da palavra fornecida no input e as imprime
 
 
 resultado= itertools.permutations(string,len(string) )

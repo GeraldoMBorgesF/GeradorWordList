d406c24a35c3035b92cc9bcf35baac417906aa17
GeraldoMBorgesF <geraldomborgesf@gmail.com>
2022-03-26 09:10:45 -0300

    permuta as letras da palavra fornecida no input e as imprime

---
+++ gwlist.py
@@ -0,0 +1,14 @@
+import itertools
+
+
+
+string = input('Palavra a ser permutada: ')
+
+# permuta as letras da palavra fornecida no input e as imprime
+
+
+resultado= itertools.permutations(string,len(string) )
+
+
+for i in resultado:
+    print(''.join(i))
